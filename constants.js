688c6e9481600a9bd04458948fb493e980cb12f3
joshuarossi <maximojoshuarossi@gmail.com>
2023-01-13 02:41:42 -0600

    adds keyboard input

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-export const GRAVITY = 0.15;
+export const GRAVITY = 0.1;
 export const RUNSPEED = 2;
 export const JUMPSPEED = -4;
-export const TERMINAL_VELOCITY = 5;
+export const TERMINAL_VELOCITY = 3;
 export const BACKGROUND_IMAGE_HEIGHT = 432;
